e75e4afd4271976d68022916b52f8236c47dab78
github username <github email address>
2019-01-13 19:10:51 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,3 +7,13 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 end
+
+
+# <% @posts.each do |post| %>
+# <div><%= link_to post.title, post_path(post) %></div>
+# <% end %>
+
+
+# <div><%= link_to post.title, post_path(post.id) %></div>
+# <div><%= link_to post.title, "/posts/#{post.id}" %></div>
+# <div><a href='<%= "/posts/#{post.id}" %>'><%= post.title %></a></div>
